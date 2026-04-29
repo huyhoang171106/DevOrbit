556e6c966e6a5809f47039a69980c364efafe6e5
Nguyen Huy Hoang <24520554@gm.uit.edu.vn>
2026-04-29 21:26:48 +0700

    chore: prepare backend env config

--- devorbit-api/src/test/java/vn/edu/uit/devorbit_api/DevorbitApiApplicationTests.java
+++ devorbit-api/src/test/java/vn/edu/uit/devorbit_api/DevorbitApiApplicationTests.java
@@ -3,7 +3,10 @@ package vn.edu.uit.devorbit_api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "app.jwt.secret=test-jwt-secret",
+    "app.github.token=test-github-token"
+})
 class DevorbitApiApplicationTests {
 
 	@Test

93f8015e91ba51424849f25e66416e6c0df533da
DuyBao <24520146@gm.uit.edu.vn>
2026-05-17 01:21:16 +0700

    chore: clean repository artifacts

--- scratch/check_pass.py
+++
@@ -1,7 +0,0 @@
-import bcrypt
-password = b"admin123"
-hash = b"$2b$12$3IpxxJtwI/zV8hYwQc8W9eWRkf.l0yocc3Di5FvA65J0QiJ395WSe"
-try:
-    print(bcrypt.checkpw(password, hash))
-except Exception as e:
-    print(f"Error: {e}")
